0f8fcdc65645371257fcd8ef789f01c2424615fa
MaleehaBhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2021-03-18 14:34:29 -0400

    Customized HTTP request

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const request = require('postman-request');
 
-const url = "http://api.weatherstack.com/current?access_key=1386bca768660f3887d5931fe7edbc80&query=37.8267,-122.4233"
+const url = "http://api.weatherstack.com/current?access_key=1386bca768660f3887d5931fe7edbc80&query=37.8267,-122.4233&units=f"
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+    const current = response.body.current
+    console.log(current.weather_descriptions[0] + ". It is currently " + current.temperature + " degrees out. It feels like " + current.feelslike + " degrees out.")
 })
 
